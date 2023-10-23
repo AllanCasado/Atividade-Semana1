e9b67c792a5dd918e6abfa6504c06bcc68dd8329
Allan Casado <allancasado04@gmail.com>
2023-10-22 22:52:04 -0300

    source code

---
+++ src/allan_casado_atv_semana_1.ino
@@ -0,0 +1,28 @@
+//allan casado
+//leitura de um sensor de humidade e temperatura utilizando o raspberry pi pico
+
+#include <DHT.h>
+
+#define DHTPIN 8    // pino do sensor dht
+#define DHTTYPE DHT22   // tipo do sensor
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial1.begin(115200);
+  Serial1.println("Hello, Raspberry Pi Pico!");
+  dht.begin();
+}
+
+void loop() {
+  delay(1500);  // faz a leitura a cada 2 segundos
+
+  // pega a temperatura e humidade no sensor
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  // exibe a temperatura e a humidade no sensor
+  Serial1.println(temperature);
+  Serial1.println(humidity);
+  Serial1.println("-------------------");
+}
